ad91f4e2aa48a53d0ca10877c026d095b2e361c7
Jesus <Merlos>
2021-06-15 20:32:11 -0400

    be first load.

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const fs = require('fs');
+
+app.get('/getjewels', (req, res) => {
+    fs.readFile('data/data.json', (err, data) => {
+        if (err) throw err;
+        res.send(JSON.parse(data));
+    })
+})
+
+app.listen(PORT, () => {
+    console.log('Listening port: ', PORT);
+})
